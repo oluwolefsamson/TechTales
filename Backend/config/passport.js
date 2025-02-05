5e077a79dc2d41b8f0584b3559aa3561c1c7d8f0
Oluwole Samson <oluwolefsamson44@gmail.com>
2025-02-05 19:18:47 +0100

    google auth implementation

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -9,7 +9,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL, // Ensure it's correct for production as well
+      callbackURL:
+        "https://techtales-1nru.onrender.com/api/auth/google/callback",
       scope: ["profile", "email"], // Scopes for profile and email permissions
     },
     async (token, tokenSecret, profile, done) => {
